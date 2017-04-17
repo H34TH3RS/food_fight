7b6b52e713a661905d75d7715ad0232a315e7a8a
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-04-17 18:24:34 -0400

    Setup basic build enviroment

--- app/client/js/game.module.js
+++ app/client/js/game.module.js
@@ -3,8 +3,35 @@
 (function() {
     'use strict';
 
-    angular.module('game', []);
+    angular.module('game', ['ui.router'])
+    .config(routerConfig);
 
+    routerConfig.$inject = ['$stateProvider'];
 
+    // $urlRouterProvider.when('', '/');
+    // $urlRouterProvider.otherwise('/not-found');
+ function routerConfig($stateProvider) {
+
+    $stateProvider
+    .state({
+      name: 'home',
+      url: '',
+      templateUrl: '/templates/home.template.html'
+    })
+    .state({
+      name: 'login',
+      url: '/login',
+      templateUrl: '/templates/login.template.html',
+      controller: 'LoginController',
+      controllerAs: 'loginCtrl'
+    })
+    .state({
+      name: 'create-user',
+      url: '/create-user',
+      templateUrl: '/templates/create_user.template.html',
+      controller: 'UpcController',
+      controllerAs: 'upcCtrl'
+    });
+}
 
 }());
