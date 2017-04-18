27b9926983be38ce782968c3b5708c490d5900d4
Sara Basile <sarabasile@Saras-Air.lan>
2017-04-18 10:23:12 -0400

    moved angular copy task

--- gruntfile.js
+++ gruntfile.js
@@ -24,16 +24,15 @@ module.exports = function(grunt){
             dest: 'public/templates/',
             expand: true
         }]
-      }
-    },
-
-    angular:{
-      files: [{
-          cwd:'node_modules/angular/',
-          src:['angular.js'],
-          dest:'public/js/',
-          expand: true
+      },
+      angular:{
+        files: [{
+            cwd:'node_modules/angular/',
+            src:['angular.min.js'],
+            dest:'public/',
+            expand: true
         }]
+      }
     },
 
     concat:{
