8a07812b1a9d4ca2878a71d24dd928b5ba61d524
Sara Basile <sarabasile@Saras-Air.lan>
2017-04-18 09:57:56 -0400

    removed remnants of login and user functions from upd controller and service.

--- app/client/js/upc.service.js
+++ app/client/js/upc.service.js
@@ -25,31 +25,8 @@
       .catch();
     }
 
-    function createUser(user) {
-      return $http({
-        url: '/api/users',
-        method: 'POST',
-        header: {
-          'Content-Type': 'application/json',
-          'Authorization': LoginService.getToken()
-        },
-        data: {
-          email: user.email,
-          username: user.username,
-          password: user.password,
-          password_confirmation: user.password_confirmation
-        }
-      })
-      .then(function handleResponse(response) {
-        return response.data;
-      })
-      .catch();
-    }
-
     return {
-      sendLogin: sendLogin,
       sendUpcData: sendUpcData,
-      createUser: createUser
     };
 
   }
