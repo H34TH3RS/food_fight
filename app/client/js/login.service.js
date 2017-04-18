54ff48fa96fa1057ac0023b5ec36d44f7cc40f41
Sara Basile <sarabasile@Saras-Air.lan>
2017-04-18 09:52:36 -0400

    added docblocks to login service

--- app/client/js/login.service.js
+++ app/client/js/login.service.js
@@ -5,10 +5,20 @@
 
     LoginService.$inject = ['$http'];
 
+  /**
+   * Creates the login service for loging in and out with an authorization token
+   * @return {Object} the object containing funcitons for use in the login controller
+   */
   function LoginService(){
 
     let token = localStorage.getItem('token');
 
+    /**
+     * Retrieves an auth token to allow the user to log in to their account.
+     * @param  {String} email    the user's email address
+     * @param  {String} password the user's password
+     * @return {String}          the authorization token
+     */
     function sendLogin(email, password) {
       return $http({
         url: '/api/users',
@@ -28,6 +38,10 @@
       });
     }
 
+    /**
+     * Removes the authorization token to log out the user
+     * @return {function} the ajax call to remove the token
+     */
     function logout() {
       return $http({
         url: '/api/users',
@@ -44,6 +58,10 @@
 
     }
 
+    /**
+     * Retrieves the token for secure access by the authorization system
+     * @return {String} the authorization token
+     */
     function getToken() {
       return token;
     }
