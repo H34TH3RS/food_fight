1afa3c2e5b018589f5dcaf1f3a323ead0bffbe9a
Sara Basile <sarabasile@Saras-Air.lan>
2017-04-18 09:46:12 -0400

    added docblocks to login controller

--- app/client/js/login.controller.js
+++ app/client/js/login.controller.js
@@ -5,12 +5,22 @@
 
   LoginController.inject = ['LoginService'];
 
+  /**
+   * Creates login controllers for allowing a user to log in to their account.
+   * @param {function} LoginService the service containing login and logout functions
+   * @return {void}
+   */
   function LoginController(LoginService){
 
     let vm = this;
 
     vm.userLogin = {};
 
+  /**
+  * Allows a user to log in to their account.
+  * @param  {Object} userLogin must contain {email: x@x.com, password: ***}
+  * @return {void}
+  */
     vm.login = function login(userLogin){
       LoginService.login(userLogin.email, userLogin.password)
       .then(function testLog(){
@@ -18,6 +28,11 @@
       });
     };
 
+
+  /**
+   * Removes the token to log out a user from the website.
+   * @return {void}
+   */
     vm.logout = function logout() {
       LoginService.logout();
     };
