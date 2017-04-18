8b6de5fec658b610f2771970b333c43b5e0b7876
Molly <mstoople@gmail.com>
2017-04-18 11:53:35 -0400

    Created get route to nutritionx api

--- spec/controllers/authorizations_controller_spec.rb
+++ spec/controllers/authorizations_controller_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Api::AuthorizationsController, type: :controller do
+  describe 'POST #create' do
+    it 'can reject an authorization' do
+      post :create, params: {
+        email: 'whatever',
+        password: 'whatever'
+      }
 
+      expect(response).to be_unauthorized
+    end
+    it 'can authorize a user' do
+      post :create, params: {
+        email: 'moose@tiydc.com',
+        password: 'password'
+      }
+      expect(response). to be_ok
+      token = JSON.parse(response.body)['auth_token']
+      expect(token).to match(/[0-f]{32}/)
+    end
+  end
 end
