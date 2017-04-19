6e986011f72cde759c3831577e0eed6b8f864e29
Sara Basile <sarabasile@Saras-Air.lan>
2017-04-19 17:02:50 -0400

    added upc lookup function

--- app/client/js/upc.service.js
+++ app/client/js/upc.service.js
@@ -7,6 +7,23 @@
 
   function UpcService($http) {
 
+    function upcLookup() {
+      return $http({
+        url: '/api/cards',
+        method: 'POST',
+        header: {
+          'Content-Type': 'application/json'
+        },
+        data:{
+          upc: upcCode.upc
+        }
+      })
+      .then(function handleResponse(response) {
+        return response.data;
+      });
+
+    }
+
     function sendUpcData(data) {
       return $http({
         url: '/api/users',
@@ -17,16 +34,15 @@
         data: {
           upc: upc //getting an error here when testing
         }
-
       })
       .then(function handleResponse(response) {
         return response.data;
-      })
-      .catch();
+      });
     }
 
     return {
       sendUpcData: sendUpcData,
+      upcLookup: upcLookup
     };
 
   }
