87a5f41756e486d08501dfe18bcc9e39e5cd324b
Molly <mstoople@gmail.com>
2017-04-20 13:56:12 -0400

    Successfully can fetch data from nutritionix and filter to specifications for a new card.

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
-class CardsController < ApplicationController
+class Api::CardsController < ApplicationController
   def create
-    NutritionixApi.new.get_nutrition_data(user_params) # TODO: need to add conditional logic to determine if a new card should be created or an old card should be pulled from memory
-    # TODO: need to decide if I need nested values for user params.
-    @card = Card.new(api_params)
+    nutrition_data = NutritionixApi.new.get_nutrition_data(user_params['upc']) # TODO: need to add conditional logic to determine if a new card should be created or an old card should be pulled from memory
+
+    @card_data = nutrition_data.slice('item_name', 'nf_calories', 'nf_total_fat', 'nf_total_carbohydrate', 'nf_dietary_fiber', 'nf_sugars', 'nf_protein', 'nf_vitamin_a_dv', 'nf_vitamin_c_dv', 'nf_calcium_dv', 'nf_iron_dv', 'nf_potassium')
+
+    @card = Card.new(@card_data)
     @card.save
   end
 
   private def user_params
-    params.require(:card).permit(:upc) # TODO: need to add validations
+    params.permit(:upc) # TODO: need to add validations
   end
 end
