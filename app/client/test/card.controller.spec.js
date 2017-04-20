5229b79e310cbdf7cb38791b841dae66c7adafb7
Sara Basile <sarabasile@Saras-Air.lan>
2017-04-20 15:21:20 -0400

    formatting

--- app/client/test/card.controller.spec.js
+++ app/client/test/card.controller.spec.js
@@ -10,14 +10,25 @@
     let card = {};
 
     beforeEach(module('game'));
+
     beforeEach(module(function($provide) {
       $provide.value('CardService', mockCardService);
     }));
+
     beforeEach(inject(function($controller) {
       mockCardService.getAllCards = function getAllCards() {
-        return [];
+        return [
+          {stats: 'testing'}
+        ];
       };
+
+      CardController = $controller('CardController');
+
     }));
+
+    it('should return an array', function() {
+      expect(CardController.getAllCards).to.be.an('array');
+    });
 
   });
 
