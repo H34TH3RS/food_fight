3c54bfb7577c766bb704fb388d16a27372ce23f2
Molly <mstoople@gmail.com>
2017-04-20 15:31:34 -0400

    Created conditional logic for saving card. Created a constant for card_keys and refactored cards_controller.

--- app/models/card_converter.rb
+++ app/models/card_converter.rb
@@ -1,22 +1,21 @@
 # frozen_string_literal: true
+CARD_KEYS = {
+  'item_name' => :food_name,
+  'nf_calories' => :energy,
+  'nf_total_fat' => :physical_resistance_debuff,
+  'nf_total_carbohydrate' => :health,
+  'nf_dietary_fiber' => :accuracy,
+  'nf_sugars' => :energy_debuff,
+  'nf_protein' => :strength,
+  'nf_vitamin_a_dv' => :accuracy_buff,
+  'nf_vitamin_c_dv' => :health_buff,
+  'nf_calcium_dv' => :defense,
+  'nf_iron_dv' => :attack_buff,
+  'nf_potassium' => :cleanse
+}
 
 class CardConverter
   def convert!(nutrition_data)
-    card_keys = {
-      'item_name' => 'food_name',
-      'nf_calories' => 'energy',
-      'nf_total_fat' => 'enemy_health_debuff',
-      'nf_total_carbohydrate' => 'health',
-      'nf_dietary_fiber' => 'base_accuracy',
-      'nf_sugars' => 'enemy_energy_debuff',
-      'nf_protein' => 'strength',
-      'nf_vitamin_a_dv' => 'accuracy',
-      'nf_vitamin_c_dv' => 'health_buff',
-      'nf_calcium_dv' => 'defense',
-      'nf_iron_dv' => 'attack',
-      'nf_potassium' => 'cleanse'
-    }
-
-    nutrition_data.map { |key, value| [card_keys[key], value] }
+    nutrition_data.map { |key, value| [CARD_KEYS[key], value] }.to_h
   end
 end
