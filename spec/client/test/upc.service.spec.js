4f4cff3e2b57f4efb0155fed4dbec501b6044279
Administrator <administrator@AdminisatorsMBP.lan>
2017-04-26 10:09:25 -0400

    moved the test folder to spec folder

---
+++ spec/client/test/upc.service.spec.js
@@ -0,0 +1,22 @@
+(function() {
+  'use strict';
+
+  let expect = chai.expect;
+
+  describe('upc service', function(){
+
+    let UpcService;
+    let mockUpcService = {};//
+
+    beforeEach(module('game'));
+
+    beforeEach(inject(function(_UpcService_){
+      UpcService = _UpcService_;
+    }));
+
+    it('should expect the function to be function', function() {
+      expect(UpcService.sendUpcData).to.be.a('function');
+    });
+  });
+
+}());
