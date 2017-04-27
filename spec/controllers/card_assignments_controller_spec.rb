3cabc1710eb680de3465d017af7f4ebaf0d323d0
Molly <mstoople@gmail.com>
2017-04-27 15:24:16 -0400

    Created test to ensure the array of player card data is sent back in valid json.

--- spec/controllers/card_assignments_controller_spec.rb
+++ spec/controllers/card_assignments_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe CardAssignmentsController, type: :controller do
-
+RSpec.describe Api::CardAssignmentsController, type: :controller do
+  describe 'GET #index' do
+    it "can list all of a player's cards" do
+      current_user = users(:russell)
+      request.headers['HTTP_AUTHORIZATION'] = "token #{current_user.auth_token}"
+      get :index
+      expect(response).to be_success
+      expect(JSON.parse(response.body)).to eq([])
+    end
+  end
 end
