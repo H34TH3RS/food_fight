cd62063baab5286a6bec5caae5fe1e06804dbb42
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-04-27 10:39:46 -0400

    fixed some margins

--- spec/client/test/game.controller.spec.js
+++ spec/client/test/game.controller.spec.js
@@ -2,9 +2,6 @@
   'use strict';
 
   let expect = chai.expect;
-  let vm = this;
-  // vm.playerHealth = 30;
-  let basicPlayerHeath = 100;
 
   describe ('GameController', function(){
       let GameController;
--- spec/client/test/game.service.spec.js
+++ spec/client/test/game.service.spec.js
@@ -13,44 +13,29 @@
       GameService = _GameService_;
     }));
 
+    it('should be return 3', function() {
+      expect(GameService.getBots().length).to.equal(3);
+      expect(GameService.getTreasures().length).to.equal(3);
+      expect(GameService.getEvents().length).to.equal(3);
+    });
 
-      it('should be return 3', function() {
-        expect(GameService.getBots().length).to.equal(3);
-      });
+    it('should be a function', function() {
+      expect(GameService.getUserCard).to.be.a('function');
+      expect(GameService.getBots).to.be.a('function');
+      expect(GameService.getTreasures).to.be.a('function');
+      expect(GameService.getEvents).to.be.a('function');
+    });
 
-      it('should be return 4', function() {
-        expect(GameService.getTreasures().length).to.equal(3);
-      });
+    it('should return an array', function() {
+      expect(GameService.getBots()).to.be.an('array');
+      expect(GameService.getTreasures()).to.be.an('array');
+      expect(GameService.getEvents()).to.be.an('array');
+    });
 
-      it('should be return 2', function() {
-        expect(GameService.getEvents().length).to.equal(3);
-      });
-
-      it('should be a function', function() {
-        expect(GameService.getUserCard).to.be.a('function');
-        expect(GameService.getBots).to.be.a('function');
-        expect(GameService.getTreasures).to.be.a('function');
-        expect(GameService.getEvents).to.be.a('function');
-      });
-
-      it('should return an array', function() {
-        expect(GameService.getBots()).to.be.an('array');
-        expect(GameService.getTreasures()).to.be.an('array');
-        expect(GameService.getEvents()).to.be.an('array');
-      });
-
-      it('should return an object', function() {
-        expect(GameService.getUserCard()).to.be.an('object');
-
-      });
-
-
-
-
-
+    it('should return an object', function() {
+      expect(GameService.getUserCard()).to.be.an('object');
+    });
 
   });
-
-
 
 }());
